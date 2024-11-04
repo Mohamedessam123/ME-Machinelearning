35f91128aa81f347fa4097a8aaef445f39bc8e57
Mohamedessam123 <90540212+Mohamedessam123@users.noreply.github.com>
2024-11-04 04:12:08 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,4 @@ with st.expander('Data'):
   y
   st.table(df.iloc[0:10])
 with st.expander('Data Visualization'):
-  st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g')
+  st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
