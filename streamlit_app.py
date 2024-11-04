251dac751ebe17d8ac112fdf02b0ee06d10c1bda
Mohamedessam123 <90540212+Mohamedessam123@users.noreply.github.com>
2024-11-04 03:18:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title('🤖 Machine-Learning App ..')
 
 st.info('This is app Building a machine-Learning Model!')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
-df
+df.head()
