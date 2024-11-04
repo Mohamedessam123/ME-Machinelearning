70d610310c6596875ec15af34810a4078506a757
Mohamedessam123 <90540212+Mohamedessam123@users.noreply.github.com>
2024-11-04 07:21:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,12 @@ with st.sidebar:
   bill_length_mm = st.slider('Bill Length (MM) ',32.1,59.6,40.1)
   flipper_length_mm = st.slider('Flipper length (mm)',df.flipper_length_mm.min(),231.0,200.0)
   body_mass_g = st.slider('Body mass (g)',2700.0,6300.0,4200.0)
+
+  data = {'island': island,
+          'bill_length_mm': bill_length_mm,
+          'bill_depth_mm': bill_depth_mm,
+          'flipper_length_mm': flipper_length_mm,
+          'body_mass_g': body_mass_g,
+          'sex': gender}
+  input_df = pd.DataFrame(data, index=[0])
+  input_df
