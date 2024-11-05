d8071ef908a4bc7afabd3e63067e4a84c6e2eded
Mohamedessam123 <90540212+Mohamedessam123@users.noreply.github.com>
2024-11-05 04:51:38 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -112,4 +112,11 @@ st.dataframe(df_prediction_proba,
 penguins_species = np.array(['Adelie', 'Chinstrap', 'Gentoo'])
 st.success(str(penguins_species[prediction][0]))
 
+st.markdown("""
+    <style>
+    .stApp {
+        background-color: #f0f2f6; /* Replace with your desired color */
+    }
+    </style>
+    """, unsafe_allow_html=True)
   
